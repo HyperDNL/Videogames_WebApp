aa9d980595bc10637dc55ac7e596d0f2e44e69e3
Daniel López <hyperdaniel579exe@gmail.com>
2023-06-18 21:23:04 -0600

    routes for videogames

---
+++ backend/src/routes/videogames.routes.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import {
+  createVideogame,
+  getVideogames,
+  searchByQuery,
+  getVideogame,
+  updateVideogame,
+  deleteVideogame,
+  deleteThumbnail,
+} from "../controllers/videogames.controllers.js";
+
+const router = Router();
+
+router.post("/", createVideogame);
+
+router.get("/", getVideogames);
+
+router.get("/search", searchByQuery);
+
+router.get("/:id", getVideogame);
+
+router.put("/:id", updateVideogame);
+
+router.delete("/:id", deleteVideogame);
+
+router.delete("/:id/thumbnails/:thumbnailId", deleteThumbnail);
+
+export default router;
