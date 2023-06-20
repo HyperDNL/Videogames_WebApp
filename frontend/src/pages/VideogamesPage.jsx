4346d75a25080a23ce37a989481d9a4444f9850f
Daniel López <hyperdaniel579exe@gmail.com>
2023-06-20 04:24:17 -0600

    page for videogames implemented

--- frontend/src/pages/VideogamesPage.jsx
+++ frontend/src/pages/VideogamesPage.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+import { useVideogames } from "../context/videogamesContext";
+import Cover from "../components/Cover";
+
+const CoverGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 16px;
+  padding: 16px;
+`;
 
 const VideogamesPage = () => {
-  return <div>Videogames Page</div>;
+  const { videogames } = useVideogames();
+
+  return (
+    <CoverGrid>
+      {videogames.map(({ _id, covers }) => (
+        <Cover key={_id} id={_id} url={covers.cover.url} />
+      ))}
+    </CoverGrid>
+  );
 };
 
 export default VideogamesPage;
