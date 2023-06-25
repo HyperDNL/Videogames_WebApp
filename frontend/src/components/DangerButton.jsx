34248d291737590a79d3d257cc6a5b2f342d3b00
Daniel López <hyperdaniel579exe@gmail.com>
2023-06-25 12:35:40 -0600

    component for danger button was created

---
+++ frontend/src/components/DangerButton.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+
+const DangerButtonField = styled.button`
+  padding: 8px 16px;
+  background-color: #ff3b30;
+  color: #ffffff;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+  width: 100%;
+
+  &:hover {
+    background-color: #ff5f55;
+  }
+
+  @media (min-width: 768px) {
+    padding: 8px 12px;
+    max-width: 200px;
+  }
+`;
+
+const DangerButton = ({ type, onClick, children }) => {
+  return (
+    <DangerButtonField type={type} onClick={onClick}>
+      {children}
+    </DangerButtonField>
+  );
+};
+
+export default DangerButton;
