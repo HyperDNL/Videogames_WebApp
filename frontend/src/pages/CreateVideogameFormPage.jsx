5dd7d3323fe00e545484cf217a42929771b3ebb5
Daniel López <hyperdaniel579exe@gmail.com>
2023-06-25 04:35:29 -0600

    implementation of the form to create videogames

--- frontend/src/pages/CreateVideogameFormPage.jsx
+++ frontend/src/pages/CreateVideogameFormPage.jsx
@@ -1,7 +1,288 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { useVideogames } from "../context/videogamesContext";
+
+const Input = styled.input`
+  padding: 8px;
+  border: 1px solid #3b88c3;
+  border-radius: 4px;
+  font-size: 16px;
+  width: 100%;
+  margin-bottom: 8px;
+  background-color: #24282b;
+  color: #a8acaf;
+  transition: border-color 0.3s ease-in-out;
+  box-sizing: border-box;
+
+  &:focus {
+    border-color: #5fa4d6;
+    outline: none;
+  }
+
+  &::placeholder {
+    color: #a8acaf;
+  }
+`;
+
+const Button = styled.button`
+  padding: 8px 16px;
+  background-color: #3b88c3;
+  color: #ffffff;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+  width: 100%;
+
+  &:hover {
+    background-color: #5fa4d6;
+  }
+
+  @media (min-width: 768px) {
+    padding: 8px 12px;
+    max-width: 200px;
+  }
+`;
+
+const SecondaryButton = styled.button`
+  padding: 8px 16px;
+  background-color: #707b7c;
+  color: #ffffff;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+  width: 100%;
+
+  &:hover {
+    background-color: #95a0a1;
+  }
+
+  @media (min-width: 768px) {
+    padding: 8px 12px;
+    max-width: 200px;
+  }
+`;
+
+const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 16px;
+  width: 100%;
+
+  @media (min-width: 768px) {
+    flex-direction: column;
+    align-items: flex-start;
+    margin-bottom: 16px;
+  }
+`;
+
+const Container = styled.div`
+  margin: 16px;
+`;
+
+const FormContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  max-width: 800px;
+`;
 
 const CreateVideogameFormPage = () => {
-  return <div>Create Videogame Form Page</div>;
+  const { createVideogame } = useVideogames();
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [developers, setDevelopers] = useState([""]);
+  const [platforms, setPlatforms] = useState([""]);
+  const [genres, setGenres] = useState([""]);
+  const [year, setYear] = useState("");
+  const [cover, setCover] = useState(null);
+  const [landscape, setLandscape] = useState(null);
+  const [thumbnails, setThumbnails] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("description", description);
+    formData.append("year", year);
+
+    developers.forEach((developer) => {
+      formData.append("developers", developer);
+    });
+
+    platforms.forEach((platform) => {
+      formData.append("platforms", platform);
+    });
+
+    genres.forEach((genre) => {
+      formData.append("genres", genre);
+    });
+
+    formData.append("cover", cover);
+    formData.append("landscape", landscape);
+
+    thumbnails.forEach((thumbnail) => {
+      formData.append("thumbnails", thumbnail);
+    });
+
+    try {
+      await createVideogame(formData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleAddInput = (setState) => {
+    setState((prev) => [...prev, ""]);
+  };
+
+  const handleInputChange = (index, setState, e) => {
+    const value = e.target.value;
+    setState((prev) => {
+      const newState = [...prev];
+      newState[index] = value;
+      return newState;
+    });
+  };
+
+  const handleFileChange = (setState, e) => {
+    const file = e.target.files[0];
+    setState(file);
+  };
+
+  const handleThumbnailChange = (e) => {
+    const files = Array.from(e.target.files);
+    setThumbnails(files);
+  };
+
+  return (
+    <Container>
+      <FormContainer>
+        <Form onSubmit={handleSubmit}>
+          <Label>
+            <span>Title:</span>
+            <Input
+              type="text"
+              placeholder="Videogame title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </Label>
+
+          <Label>
+            <span>Description:</span>
+            <Input
+              type="text"
+              placeholder="Videogame description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </Label>
+
+          <Label>
+            <span>Developers:</span>
+            {developers.map((developer, index) => (
+              <Input
+                key={index}
+                type="text"
+                placeholder="Developer"
+                value={developer}
+                onChange={(e) => handleInputChange(index, setDevelopers, e)}
+              />
+            ))}
+            <SecondaryButton
+              type="button"
+              onClick={() => handleAddInput(setDevelopers)}
+            >
+              Add Developer
+            </SecondaryButton>
+          </Label>
+
+          <Label>
+            <span>Platforms:</span>
+            {platforms.map((platform, index) => (
+              <Input
+                key={index}
+                type="text"
+                placeholder="Platform"
+                value={platform}
+                onChange={(e) => handleInputChange(index, setPlatforms, e)}
+              />
+            ))}
+            <SecondaryButton
+              type="button"
+              onClick={() => handleAddInput(setPlatforms)}
+            >
+              Add Platform
+            </SecondaryButton>
+          </Label>
+
+          <Label>
+            <span>Genres:</span>
+            {genres.map((genre, index) => (
+              <Input
+                key={index}
+                type="text"
+                placeholder="Genre"
+                value={genre}
+                onChange={(e) => handleInputChange(index, setGenres, e)}
+              />
+            ))}
+            <SecondaryButton
+              type="button"
+              onClick={() => handleAddInput(setGenres)}
+            >
+              Add Genre
+            </SecondaryButton>
+          </Label>
+
+          <Label>
+            <span>Year:</span>
+            <Input
+              type="text"
+              placeholder="Release year"
+              value={year}
+              onChange={(e) => setYear(e.target.value)}
+            />
+          </Label>
+
+          <Label>
+            <span>Cover:</span>
+            <Input
+              type="file"
+              onChange={(e) => handleFileChange(setCover, e)}
+            />
+          </Label>
+
+          <Label>
+            <span>Landscape:</span>
+            <Input
+              type="file"
+              onChange={(e) => handleFileChange(setLandscape, e)}
+            />
+          </Label>
+
+          <Label>
+            <span>Thumbnails:</span>
+            <Input type="file" multiple onChange={handleThumbnailChange} />
+          </Label>
+
+          <Button type="submit">Create</Button>
+        </Form>
+      </FormContainer>
+    </Container>
+  );
 };
 
 export default CreateVideogameFormPage;
