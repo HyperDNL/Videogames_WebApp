00982d88fdad13b323d405f9dd37123dde793205
Daniel López <hyperdaniel579exe@gmail.com>
2023-07-31 13:20:11 -0600

    Updated global variables

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -7,3 +7,5 @@ export const WHITELISTED_DOMAINS = process.env.WHITELISTED_DOMAINS;
 export const CLOUD_NAME = process.env.CLOUD_NAME;
 export const API_KEY = process.env.API_KEY;
 export const API_SECRET = process.env.API_SECRET;
+export const MAX_IMAGE_SIZE_MB = 5;
+export const ALLOWED_IMAGE_EXTENSIONS = [".jpg", ".jpeg", ".png", ".webp"];
